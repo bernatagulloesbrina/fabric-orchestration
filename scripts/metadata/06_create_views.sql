9a1d32dc8811fdc4ac6cae354c1e7eb52264f762
Bernat Agulló Roselló <bernat.agullo@esbrina-ba.com>
2026-07-06 21:43:36 +0900

    view improvement

--- scripts/metadata/06_create_views.sql
+++ scripts/metadata/06_create_views.sql
@@ -22,14 +22,24 @@ GO
 
 CREATE VIEW dbo.vw_executions AS
 SELECT
-    execution_id,
-    job_name,
-    CAST(start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS start_time,
-    CAST(end_time   AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS end_time,
-    CAST(created_at AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS created_at,
-    result,
-    error_message
-FROM dbo.executions;
+    e.execution_id,
+    e.job_name,
+    CAST(e.start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS start_time,
+    CAST(e.end_time   AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS end_time,
+    CAST(e.created_at AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS created_at,
+    CAST(CAST(e.start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7)) AS DATE) AS start_date,
+    CAST(
+        DATEADD(
+            HOUR,
+            DATEDIFF(HOUR, 0, CAST(e.start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Central European Standard Time' AS DATETIME2(7))),
+            0
+        ) AS DATETIME2(7)
+    ) AS start_hour,
+    j.object_type,
+    e.result,
+    e.error_message
+FROM dbo.executions e
+LEFT JOIN dbo.jobs j ON j.job_name = e.job_name;
 GO
 
 CREATE VIEW dbo.vw_refresh_jobs AS
