c6aafc65abcf105b01fb337de53d58d38a2fc47b
Bernat Agulló Roselló <bernat.agullo@esbrina-ba.com>
2026-07-19 14:39:39 +0900

    refactor: update views and stored procedures for refresh cutoff management

--- Metadata.SQLDatabase/dbo/Views/error_jobs.sql
+++ Metadata.SQLDatabase/dbo/Views/error_jobs.sql
@@ -1,14 +1,11 @@
 CREATE VIEW [dbo].[error_jobs]
 AS
-WITH cutoff AS (
-    SELECT DATEADD(HOUR, 22, CAST(DATEADD(DAY, -1, CAST(GETUTCDATE() AS DATE)) AS DATETIME2(7))) AS cutoff_utc
-)
 SELECT
     e.job_name,
     MAX(e.end_time) AS last_success_end_time,
     COUNT_BIG(*) AS successful_executions_since_cutoff
 FROM dbo.executions AS e
-CROSS JOIN cutoff AS c
+CROSS JOIN dbo.vw_refresh_cutoff AS c
 WHERE e.result = 'Error'
   AND e.end_time >= c.cutoff_utc
 GROUP BY e.job_name;
